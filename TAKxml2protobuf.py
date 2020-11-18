f59587279958b0d19c0582cf1f25ee3207b40c44
Delta Bravo One Five <deltabravo15ga@gmail.com>
2020-11-18 18:01:07 -0500

    updated to use Python protobuf module
    Takes XML on STDIN and returns the binary protobuf on STDOUT

--- TAKxml2protobuf.py
+++ TAKxml2protobuf.py
@@ -4,158 +4,132 @@ import time
 from datetime import datetime
 import sys
 import re
+import takmessage_pb2
 
 # Read XML from STDIN and parse
 xml = sys.stdin.read()
 
+# Read XML from local file
+# with open('chat.xml', 'r') as file:
+#	xml = file.read()
+#	file.close()
+
 cot = untangle.parse(xml)
 
-# Clear contents of output file
-outtxt = open('out.txt', 'w')
-outtxt.close()
-# Open output file for writing
-outtxt = open("out.txt", "a")
-# If the CoT is GeoChat, include the contact's UID in {takControl}
-outtxt.write("takControl {\n")
+takMessage = takmessage_pb2.TakMessage()
+takControl = takMessage.takControl
+cotEvent = takMessage.cotEvent
+
 if "GeoChat." in cot.event['uid']:
-        contactUid = cot.event['uid'].split(".")[1]
-        outtxt.write("  contactUid: \"" + contactUid + "\"\n")
-outtxt.write("}\n")
+	takControl.contactUid = cot.event['uid'].split(".")[1]
 
 # If XML contains an event element, write the attributes
-if 'event' in dir(cot):
-        outtxt.write("cotEvent {\n")
-        if cot.event['type']:
-                outtxt.write("  type: \"" + cot.event['type'] + "\"\n")
-                epoch = datetime.utcfromtimestamp(0)
+if 'event' in dir(cot):		
+	if cot.event['type']:
+		cotEvent.type = cot.event['type']
 
-        if cot.event['access']:
-                outtxt.write("  access: \"" + cot.event['access'] + "\"\n")
+	if cot.event['access']:
+		cotEvent.access = cot.event['access']
 
-        if cot.event['qos']:
-                outtxt.write("  qos: \"" + cot.event['qos'] + "\"\n")
+	if cot.event['qos']:
+		cotEvent.qos = cot.event['qos']
 
-        if cot.event['opex']:
-                outtxt.write("  opex: \"" + cot.event['opex'] + "\"\n")
+	if cot.event['opex']:
+		cotEvent.opex = cot.event['opex']
 
-        if cot.event['uid']:
-                outtxt.write("  uid: \"" + cot.event['uid'] + "\"\n")
+	if cot.event['uid']:
+		cotEvent.uid = cot.event['uid']
 
-        if cot.event['time']:
-                sendTime = datetime.strptime(cot.event['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
-                sendTime = str(int(sendTime.timestamp() * 1000))
-                outtxt.write("  sendTime: " + sendTime + "\n")
+	if cot.event['time']:
+		sendTime = datetime.strptime(cot.event['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+		sendTime = int(sendTime.timestamp() * 1000)
+		cotEvent.sendTime = sendTime
+				
+	if cot.event['start']:
+		startTime = datetime.strptime(cot.event['start'], '%Y-%m-%dT%H:%M:%S.%fZ')
+		startTime = int(startTime.timestamp() * 1000)
+		cotEvent.startTime = startTime
 
-        if cot.event['start']:
-                startTime = datetime.strptime(cot.event['start'], '%Y-%m-%dT%H:%M:%S.%fZ')
-                startTime = str(int(startTime.timestamp() * 1000))
-                outtxt.write("  startTime: " + startTime + "\n")
+	if cot.event['stale']:
+		staleTime = datetime.strptime(cot.event['stale'], '%Y-%m-%dT%H:%M:%S.%fZ')
+		staleTime = int(staleTime.timestamp() * 1000)
+		cotEvent.staleTime = staleTime
 
-        if cot.event['stale']:
-                staleTime = datetime.strptime(cot.event['stale'], '%Y-%m-%dT%H:%M:%S.%fZ')
-                staleTime = str(int(staleTime.timestamp() * 1000))
-                outtxt.write("  staleTime: " + staleTime + "\n")
+	if cot.event['how']:
+		cotEvent.how = cot.event['how']
 
-        if cot.event['how']:
-                outtxt.write("  how: \"" + cot.event['how'] + "\"\n")
+	# If the event element includes a point child, write the attributes
+	if 'point' in dir(cot.event):
 
-        # If the event element includes a point child, write the attributes
-        if 'point' in dir(cot.event):
-                if cot.event.point['lat']:
-                        outtxt.write("  lat: " + cot.event.point['lat'] + "\n")
+		if cot.event.point['lat']:
+			cotEvent.lat = float(cot.event.point['lat'])
 
-                if cot.event.point['lon']:
-                        outtxt.write("  lon: " + cot.event.point['lon'] + "\n")
+		if cot.event.point['lon']:
+			cotEvent.lon = float(cot.event.point['lon'])
 
-                if cot.event.point['hae']:
-                        outtxt.write("  hae: " + cot.event.point['hae'] + "\n")
+		if cot.event.point['hae']:
+			cotEvent.hae = float(cot.event.point['hae'])
 
-                if cot.event.point['ce']:
-                        outtxt.write("  ce: " + cot.event.point['ce'] + "\n")
+		if cot.event.point['ce']:
+			cotEvent.ce = float(cot.event.point['ce'])
 
-                if cot.event.point['le']:
-                        outtxt.write("  le: " + cot.event.point['le'] + "\n")
+		if cot.event.point['le']:
+			cotEvent.le = float(cot.event.point['le'])
 
-                        # If the XML includes a Detail element, write the attributes
-                        if 'detail' in dir(cot.event):
-                                outtxt.write("  detail {\n")
+	# If the XML includes a Detail element, write the attributes
+	if 'detail' in dir(cot.event):
+		detail = cotEvent.detail
+		if "GeoChat." in cot.event['uid']:
+			pattern = "<detail>(.*?)</detail>"
+			xmldetailstr = re.search(pattern, xml).group(1)
+#			xmldetailstr = xmldetailstr.replace("\'", "\\\'").replace("\"","\\\'")
+			detail.xmlDetail = xmldetailstr
 
-                                if "GeoChat." in cot.event['uid']:
-                                        pattern = "<detail>(.*?)</detail>"
-                                        xmldetailstr = re.search(pattern, xml).group(1)
-                                        xmldetailstr = xmldetailstr.replace("\'", "\\\'").replace("\"","\\\'")
-                                        outtxt.write("    xmlDetail: \"" + xmldetailstr + "\"\n")
+		if 'contact' in dir(cot.event.detail):
+			if cot.event.detail.contact['endpoint']:
+				detail.contact.endpoint = cot.event.detail.contact['endpoint']
 
-                                if 'contact' in dir(cot.event.detail):
-                                        outtxt.write("    contact {\n")
+			if cot.event.detail.contact['callsign']:
+				detail.contact.callsign = cot.event.detail.contact['callsign']
 
-                                        if cot.event.detail.contact['endpoint']:
-                                                outtxt.write("      endpoint: \"" + cot.event.detail.contact['endpoint'] + "\"\n")
+			if '__group' in dir(cot.event.detail):
+				if cot.event.detail.__group['name']:
+					detail.__group.name = cot.event.detail.__group['name']
 
-                                        if cot.event.detail.contact['callsign']:
-                                                outtxt.write("      callsign: \"" + cot.event.detail.contact['callsign'] + "\"\n")
+				if cot.event.detail.__group['role']:
+					detail.__group.role = cot.event.detail.__group['role']
 
-                                        outtxt.write("    }\n")
+				if 'precisionlocation' in dir(cot.event.detail):
+					if cot.event.detail.precisionlocation['geopointsrc']:
+						detail.precisionlocation.geopointsrc = cot.detail.precisionlocation['geopointsrc']
+						if cot.event.detail.precisionlocation['altsrc']:
+							detail.precisionlocation.altsrc = cot.detail.precisionlocation['altsrc']
 
-                                if '__group' in dir(cot.event.detail):
-                                        outtxt.write("    group {\n")
-                                        if cot.event.detail.__group['name']:
-                                                outtxt.write("      name: \"" + cot.event.detail.__group['name'] + "\"\n")
+		if 'status' in dir(cot.event.detail):
+			if cot.event.detail.status['battery']:
+				detail.status.battery = cot.event.detail.status['battery']
 
-                                        if cot.event.detail.__group['role']:
-                                                outtxt.write("      role: \"" + cot.event.detail.__group['role'] + "\"\n")
+		if 'takv' in dir(cot.event.detail):
+			if cot.event.detail.takv['device']:
+				detail.takv.device = cot.event.detail.takv['device']
 
-                                        outtxt.write("    }\n")
+			if cot.event.detail.takv['platform']:
+				detail.takv.platform = cot.event.detail.takv['platform']
 
-                                if 'precisionlocation' in dir(cot.event.detail):
-                                        outtxt.write("    precisionlocation {\n")
-                                        if cot.event.detail.precisionlocation['geopointsrc']:
-                                                outtxt.write("      geopointsrc: \"" + cot.detail.precisionlocation['geopointsrc'] + "\"\n")
+			if cot.event.detail.takv['os']:
+				detail.takv.os = cot.event.detail.takv['os']
 
-                                        if cot.event.detail.precisionlocation['altsrc']:
-                                                outtxt.write("      altscr: \"" + cot.detail.precisionlocation['altsrc'] + "\"\n")
+			if cot.event.detail.takv['version']:
+				detail.takv.version = cot.event.detail.takv['version']
 
-                                        outtxt.write("    }\n")
+		if 'track' in dir(cot.event.detail):
+			if cot.event.detail.track['speed']:
+				detail.track.speed = cot.event.detail.track['speed']
 
-                                if 'status' in dir(cot.event.detail):
-                                        outtxt.write("    status {\n")
-                                        if cot.event.detail.status['battery']:
-                                                outtxt.write("      battery: " + cot.event.detail.status['battery'] + "\n")
-
-                                        outtxt.write("    }\n")
-                                if 'takv' in dir(cot.event.detail):
-                                        outtxt.write("    takv {\n")
-                                        if cot.event.detail.takv['device']:
-                                                outtxt.write("      device : \"" + cot.event.detail.takv['device'] + "\"\n")
-
-                                        if cot.event.detail.takv['platform']:
-                                                outtxt.write("      platform : \"" + cot.event.detail.takv['platform'] + "\"\n")
-
-                                        if cot.event.detail.takv['os']:
-                                                outtxt.write("      os: \"" + cot.event.detail.takv['os'] + "\"\n")
-
-                                        if cot.event.detail.takv['version']:
-                                                outtxt.write("      version: \"" + cot.event.detail.takv['version'] + "\"\n")
-
-                                                outtxt.write("    }\n")
-
-                                        if cot.event.detail.track._name:
-                                                outtxt.write("    track {\n")
-                                                if cot.event.detail.track['speed']:
-                                                        outtxt.write("      speed: " +  cot.event.detail.track['speed'] + "\n")
-
-                                                if cot.event.detail.track['course']:
-                                                        outtxt.write("      course: " + cot.event.detail.track['course'] + "\n")
-
-
-                                                outtxt.write("    }\n")
-
-                outtxt.write("  }\n")
-
-        outtxt.write("}")
-
-outtxt.close()
-outtxt = open('out.txt', 'r')
-print(outtxt.read())
-outtxt.close()
-os.remove('out.txt')
+			if cot.event.detail.track['course']:
+				detail.track.couse = cot.event.detail.track['course']
+				
+headerByteArray = bytearray(b'\xbf\x01\xbf')
+takMessageByteArray = bytearray(takMessage.SerializeToString())
+outputByteArray = headerByteArray + takMessageByteArray
+sys.stdout.buffer.write(outputByteArray)
