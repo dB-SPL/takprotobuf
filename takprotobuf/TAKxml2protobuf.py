4c2687cc5e54db0ea8f20a20d9ca72594511f325
Delta Bravo One Five <deltabravo15ga@gmail.com>
2020-11-19 11:52:16 -0500

    Update TAKxml2protobuf.py

--- takprotobuf/TAKxml2protobuf.py
+++ takprotobuf/TAKxml2protobuf.py
@@ -1,135 +1,134 @@
-import os
 import untangle
 import time
 from datetime import datetime
 import sys
 import re
-import takmessage_pb2
+from .proto import TakMessage
 
 # Read XML from STDIN and parse
-xml = sys.stdin.read()
+#str = sys.stdin.read()
 
 # Read XML from local file
-# with open('chat.xml', 'r') as file:
+#with open('chat.xml', 'r') as file:
 #	xml = file.read()
 #	file.close()
 
-cot = untangle.parse(xml)
+def renderProto(xml):
+	cot = untangle.parse(xml)
+	takMessage = TakMessage()
+	takControl = takMessage.takControl
+	cotEvent = takMessage.cotEvent
 
-takMessage = takmessage_pb2.TakMessage()
-takControl = takMessage.takControl
-cotEvent = takMessage.cotEvent
+	if "GeoChat." in cot.event['uid']:
+		takControl.contactUid = cot.event['uid'].split(".")[1]
 
-if "GeoChat." in cot.event['uid']:
-	takControl.contactUid = cot.event['uid'].split(".")[1]
+	# If XML contains an event element, write the attributes
+	if 'event' in dir(cot):		
+		if cot.event['type']:
+			cotEvent.type = cot.event['type']
 
-# If XML contains an event element, write the attributes
-if 'event' in dir(cot):		
-	if cot.event['type']:
-		cotEvent.type = cot.event['type']
+		if cot.event['access']:
+			cotEvent.access = cot.event['access']
 
-	if cot.event['access']:
-		cotEvent.access = cot.event['access']
+		if cot.event['qos']:
+			cotEvent.qos = cot.event['qos']
 
-	if cot.event['qos']:
-		cotEvent.qos = cot.event['qos']
+		if cot.event['opex']:
+			cotEvent.opex = cot.event['opex']
 
-	if cot.event['opex']:
-		cotEvent.opex = cot.event['opex']
+		if cot.event['uid']:
+			cotEvent.uid = cot.event['uid']
 
-	if cot.event['uid']:
-		cotEvent.uid = cot.event['uid']
+		if cot.event['time']:
+			sendTime = datetime.strptime(cot.event['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+			sendTime = int(sendTime.timestamp() * 1000)
+			cotEvent.sendTime = sendTime
+					
+		if cot.event['start']:
+			startTime = datetime.strptime(cot.event['start'], '%Y-%m-%dT%H:%M:%S.%fZ')
+			startTime = int(startTime.timestamp() * 1000)
+			cotEvent.startTime = startTime
 
-	if cot.event['time']:
-		sendTime = datetime.strptime(cot.event['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
-		sendTime = int(sendTime.timestamp() * 1000)
-		cotEvent.sendTime = sendTime
-				
-	if cot.event['start']:
-		startTime = datetime.strptime(cot.event['start'], '%Y-%m-%dT%H:%M:%S.%fZ')
-		startTime = int(startTime.timestamp() * 1000)
-		cotEvent.startTime = startTime
+		if cot.event['stale']:
+			staleTime = datetime.strptime(cot.event['stale'], '%Y-%m-%dT%H:%M:%S.%fZ')
+			staleTime = int(staleTime.timestamp() * 1000)
+			cotEvent.staleTime = staleTime
 
-	if cot.event['stale']:
-		staleTime = datetime.strptime(cot.event['stale'], '%Y-%m-%dT%H:%M:%S.%fZ')
-		staleTime = int(staleTime.timestamp() * 1000)
-		cotEvent.staleTime = staleTime
+		if cot.event['how']:
+			cotEvent.how = cot.event['how']
 
-	if cot.event['how']:
-		cotEvent.how = cot.event['how']
+		# If the event element includes a point child, write the attributes
+		if 'point' in dir(cot.event):
 
-	# If the event element includes a point child, write the attributes
-	if 'point' in dir(cot.event):
+			if cot.event.point['lat']:
+				cotEvent.lat = float(cot.event.point['lat'])
 
-		if cot.event.point['lat']:
-			cotEvent.lat = float(cot.event.point['lat'])
+			if cot.event.point['lon']:
+				cotEvent.lon = float(cot.event.point['lon'])
 
-		if cot.event.point['lon']:
-			cotEvent.lon = float(cot.event.point['lon'])
+			if cot.event.point['hae']:
+				cotEvent.hae = float(cot.event.point['hae'])
 
-		if cot.event.point['hae']:
-			cotEvent.hae = float(cot.event.point['hae'])
+			if cot.event.point['ce']:
+				cotEvent.ce = float(cot.event.point['ce'])
 
-		if cot.event.point['ce']:
-			cotEvent.ce = float(cot.event.point['ce'])
+			if cot.event.point['le']:
+				cotEvent.le = float(cot.event.point['le'])
 
-		if cot.event.point['le']:
-			cotEvent.le = float(cot.event.point['le'])
+		# If the XML includes a Detail element, write the attributes
+		if 'detail' in dir(cot.event):
+			detail = cotEvent.detail
+			if "GeoChat." in cot.event['uid']:
+				pattern = "<detail>(.*?)</detail>"
+				xmldetailstr = re.search(pattern, xml).group(1)
+	#			xmldetailstr = xmldetailstr.replace("\'", "\\\'").replace("\"","\\\'")
+				detail.xmlDetail = xmldetailstr
 
-	# If the XML includes a Detail element, write the attributes
-	if 'detail' in dir(cot.event):
-		detail = cotEvent.detail
-		if "GeoChat." in cot.event['uid']:
-			pattern = "<detail>(.*?)</detail>"
-			xmldetailstr = re.search(pattern, xml).group(1)
-#			xmldetailstr = xmldetailstr.replace("\'", "\\\'").replace("\"","\\\'")
-			detail.xmlDetail = xmldetailstr
+			if 'contact' in dir(cot.event.detail):
+				if cot.event.detail.contact['endpoint']:
+					detail.contact.endpoint = cot.event.detail.contact['endpoint']
 
-		if 'contact' in dir(cot.event.detail):
-			if cot.event.detail.contact['endpoint']:
-				detail.contact.endpoint = cot.event.detail.contact['endpoint']
+				if cot.event.detail.contact['callsign']:
+					detail.contact.callsign = cot.event.detail.contact['callsign']
 
-			if cot.event.detail.contact['callsign']:
-				detail.contact.callsign = cot.event.detail.contact['callsign']
+				if '__group' in dir(cot.event.detail):
+					if cot.event.detail.__group['name']:
+						detail.__group.name = cot.event.detail.__group['name']
 
-			if '__group' in dir(cot.event.detail):
-				if cot.event.detail.__group['name']:
-					detail.__group.name = cot.event.detail.__group['name']
+					if cot.event.detail.__group['role']:
+						detail.__group.role = cot.event.detail.__group['role']
 
-				if cot.event.detail.__group['role']:
-					detail.__group.role = cot.event.detail.__group['role']
+					if 'precisionlocation' in dir(cot.event.detail):
+						if cot.event.detail.precisionlocation['geopointsrc']:
+							detail.precisionlocation.geopointsrc = cot.detail.precisionlocation['geopointsrc']
+							if cot.event.detail.precisionlocation['altsrc']:
+								detail.precisionlocation.altsrc = cot.detail.precisionlocation['altsrc']
 
-				if 'precisionlocation' in dir(cot.event.detail):
-					if cot.event.detail.precisionlocation['geopointsrc']:
-						detail.precisionlocation.geopointsrc = cot.detail.precisionlocation['geopointsrc']
-						if cot.event.detail.precisionlocation['altsrc']:
-							detail.precisionlocation.altsrc = cot.detail.precisionlocation['altsrc']
+			if 'status' in dir(cot.event.detail):
+				if cot.event.detail.status['battery']:
+					detail.status.battery = cot.event.detail.status['battery']
 
-		if 'status' in dir(cot.event.detail):
-			if cot.event.detail.status['battery']:
-				detail.status.battery = cot.event.detail.status['battery']
+			if 'takv' in dir(cot.event.detail):
+				if cot.event.detail.takv['device']:
+					detail.takv.device = cot.event.detail.takv['device']
 
-		if 'takv' in dir(cot.event.detail):
-			if cot.event.detail.takv['device']:
-				detail.takv.device = cot.event.detail.takv['device']
+				if cot.event.detail.takv['platform']:
+					detail.takv.platform = cot.event.detail.takv['platform']
 
-			if cot.event.detail.takv['platform']:
-				detail.takv.platform = cot.event.detail.takv['platform']
+				if cot.event.detail.takv['os']:
+					detail.takv.os = cot.event.detail.takv['os']
 
-			if cot.event.detail.takv['os']:
-				detail.takv.os = cot.event.detail.takv['os']
+				if cot.event.detail.takv['version']:
+					detail.takv.version = cot.event.detail.takv['version']
 
-			if cot.event.detail.takv['version']:
-				detail.takv.version = cot.event.detail.takv['version']
+			if 'track' in dir(cot.event.detail):
+				if cot.event.detail.track['speed']:
+					detail.track.speed = cot.event.detail.track['speed']
 
-		if 'track' in dir(cot.event.detail):
-			if cot.event.detail.track['speed']:
-				detail.track.speed = cot.event.detail.track['speed']
-
-			if cot.event.detail.track['course']:
-				detail.track.couse = cot.event.detail.track['course']
-				
-headerByteArray = bytearray(b'\xbf\x01\xbf')
-takMessageByteArray = bytearray(takMessage.SerializeToString())
-outputByteArray = headerByteArray + takMessageByteArray
-sys.stdout.buffer.write(outputByteArray)
+				if cot.event.detail.track['course']:
+					detail.track.couse = cot.event.detail.track['course']
+					
+	headerByteArray = bytearray(b'\xbf\x01\xbf')
+	takMessageByteArray = bytearray(takMessage.SerializeToString())
+	outputByteArray = headerByteArray + takMessageByteArray
+	return outputByteArray
