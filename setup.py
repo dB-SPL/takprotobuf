ea4476caf546c8954c453faf810f68a3e06f7f87
Greg Albrecht <github.com@undef.net>
2023-01-12 16:31:27 -0800

    adding pinned protobuf version

--- setup.py
+++ setup.py
@@ -83,5 +83,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     keywords=["Cursor on Target", "ATAK", "TAK", "CoT", "WinTAK", "iTAK"],
-    install_requires=["protobuf"]
+    install_requires=["protobuf >= 4.21.12"]
 )
